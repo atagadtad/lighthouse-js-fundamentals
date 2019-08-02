a1eecb48f6846dabb5cb92787a4a1e0b7ef4873b
atagadtad <aarontagadtad@gmail.com>
2019-08-02 20:03:41 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,14 @@
+const raining = false;
+const cold = false;
+
+if (raining) {
+  console.log(`Don't forget your umbrella!`);
+}
+
+if (cold) {
+  console.log(`Make sure you pick out a scarf!`)
+}
+
 const temperature = 15;
 
 if (temperature < 0) {
@@ -9,3 +20,18 @@ if (temperature < 0) {
 }
 
 console.log(`Now you're ready to go outside!`);
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log(`You are eligible to vote.`);
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log
+}
+
+if (!raining) {
+  console.log(`Leave your umbrella at home!`);
+}
